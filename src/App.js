9543143ef5418bc05226e14a9315b1e60a4d1388
frekenb <107190298+frekenb@users.noreply.github.com>
2022-10-12 14:40:10 +0400

    added all main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <Header />
+      <div className="main-app-part container">
+        <div className="clearfix">
+          <form className="float-left">
+            <input type="text" placeholder="enter a city" autoComplete="off" />
+            <input type="submit" className="btn btn-primary" value="Search" />
+          </form>
+          <button className="float-left btn btn-success">
+            Current location
+          </button>
+        </div>
+        <div className="weather-summary">
+          <div className="weather-summary-header pb-4">
+            <h1>Kiev</h1>
+            <div className="weather-detail-text">Tuesday 18:45</div>
+            <div className="weather-detail-text">Clouds</div>
+            <div className="row">
+              <div className="col-sm-6">
+                <div className="clearfix">
+                  <div className="float-left weather-icon"></div>
+                  <div className="weather-temp weather-temp-today">25 C</div>
+                </div>
+              </div>
+              <div className="col-sm-6">
+                <div className="weather-detail">Precipitation: 59</div>
+                <div className="weather-detail">Wind: 5,4 km/hour</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm-2">
+            <div className="forecast-day">Mon</div>
+            <div className="forecast-icon">24</div>
+            <div className="forecast-temperature">23</div>
+          </div>
+          <div className="col-sm-2">
+            <div className="forecast-day">Tue</div>
+            <div className="forecast-icon">24</div>
+            <div className="forecast-temperature">24</div>
+          </div>
+          <div className="col-sm-2">
+            {" "}
+            <div className="forecast-day">Wed</div>
+            <div className="forecast-icon">23</div>
+            <div className="forecast-temperature">23</div>
+          </div>
+          <div className="col-sm-2">
+            <div className="forecast-day">Thu</div>
+            <div className="forecast-icon">23</div>
+            <div className="forecast-temperature">23</div>
+          </div>
+          <div className="col-sm-2">
+            {" "}
+            <div className="forecast-day">Fri</div>
+            <div className="forecast-icon">23</div>
+            <div className="forecast-temperature">23</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+
+function Header() {
+  const cities = ["Kiev", "Lviv", "Mykolaiv", "Kharkiv", "Chernihiv"];
+  return (
+    <header>
+      <ul className="navigation-items d-flex flex-row bd-highlight mb-3">
+        <li className="navigation-item p-2 bd-highlight">
+          <a href="">{cities[0]}</a>
+        </li>
+        <li className="navigation-item p-2 bd-highlight">
+          <a href="">{cities[1]}</a>
+        </li>
+        <li className="navigation-item p-2 bd-highlight">
+          <a href="">{cities[2]}</a>
+        </li>
+        <li className="navigation-item p-2 bd-highlight">
+          <a href="">{cities[3]}</a>
+        </li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
