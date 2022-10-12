62bbad0a2af52458a6a9f1a0c1ef20bafe5a0c2c
frekenb <107190298+frekenb@users.noreply.github.com>
2022-10-12 14:48:47 +0400

    changed href in header.js

--- src/Header.js
+++ src/Header.js
@@ -7,16 +7,16 @@ function Header() {
     <header>
       <ul className="navigation-items d-flex flex-row bd-highlight mb-3">
         <li className="navigation-item p-2 bd-highlight">
-          <a href="#">{cities[0]}</a>
+          <a href="/">{cities[0]}</a>
         </li>
         <li className="navigation-item p-2 bd-highlight">
-          <a href="#">{cities[1]}</a>
+          <a href="/Lviv">{cities[1]}</a>
         </li>
         <li className="navigation-item p-2 bd-highlight">
-          <a href="#">{cities[2]}</a>
+          <a href="/Mykolaiv">{cities[2]}</a>
         </li>
         <li className="navigation-item p-2 bd-highlight">
-          <a href="#">{cities[3]}</a>
+          <a href="/Kharkiv">{cities[3]}</a>
         </li>
       </ul>
     </header>
